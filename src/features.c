389e12fef7cf246841b4f02718f248bda6473b54
Naoyv <yoan.vanhoudenhove@etu.estia.fr>
2025-06-16 10:58:15 +0200

    mirroir total et vertical

--- src/features.c
+++ src/features.c
@@ -1,6 +1,6 @@
 #include <estia-image.h>
 #include <stdio.h>
-#include <stdlib.h>#include <stdlib.h>
+#include <stdlib.h>
 #include "features.h"
 #include "utils.h"
 
@@ -458,6 +458,27 @@ void mirror_vertical(char *source_path){
         for (int x = 0; x < width; x++) {
             for (int c = 0; c < channel_count; c++) {
                 data_out[((height - 1 - y) * width + x) * channel_count + c] =
+                    data_in[(y * width + x) * channel_count + c];
+            }
+        }
+    }
+
+    write_image_data("image_out.bmp", data_out, new_width, new_height);
+
+    free_image_data(data_in);
+    free(data_out);
+}
+void mirror_total(char *source_path){
+    int width, height, channel_count;
+    unsigned char *data_in;
+    read_image_data(source_path, &data_in, &width, &height, &channel_count);
+    int new_width = height;
+    int new_height = width;
+    unsigned char *data_out = malloc(new_width * new_height * channel_count);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int c = 0; c < channel_count; c++) {
+                data_out[((height - 1 - y) * width + (width - 1 - x)) * channel_count + c] =
                     data_in[(y * width + x) * channel_count + c];
             }
         }
