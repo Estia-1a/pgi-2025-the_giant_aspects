e7e7b03f65bbb413a29a72997c01b48b7944dec9
Adrien DOMENGER <adrien.domenger@etu.estia.fr>
2025-06-17 22:10:57 +0200

    Scale_crop mise à jour avant test

--- src/features.c
+++ src/features.c
@@ -1,6 +1,5 @@
 #include <estia-image.h>
 #include <stdio.h>
-
 #include "features.h"
 #include "utils.h"
 
@@ -63,4 +62,25 @@ void scale_crop (char *source_path, int center_x, int center_y, int box_width, i
     int width, height, channel_count;
 
     read_image_data(source_path, &data, &width, &height, &channel_count);
+    
+int start_x = center_x - box_width / 2;
+int start_y = center_y - box_height / 2;
+if (start_x <0) start_x=0;
+if (start_y <0) start_y=0;
+if (start_x + box_width >width) box_width= width -start_x;
+if (start_y + box_width >width) box_height= height -start_y;
+
+unsigned char *cropped=malloc(box_width*box_height*channel_count);
+for (int y =0; y<box_height; y++) {
+    for (int x=0; x <box_width; x++){
+        for(int c=0; c <channel_count; c++){
+        int src_index = ((start_y+y)*width +(start_x+x))*channel_count+c;
+        int dst_index =(y*box_width+x)*channel_count+c;
+        cropped[dst_index]=data[src_index];
+        }
+    }
+    }
+    write_image_data ("image_out.bmp", cropped, box_width, box_height);
+    free(cropped);
 }
+
--- src/main.c
+++ src/main.c
@@ -40,6 +40,17 @@ int main(int argc, char **argv) {
   if (strncmp(configuration.command, "color_invert", 12)== 0) {
     color_invert(configuration.filenames[0]); 
   }
+
+   if (strncmp(configuration.command, "scale_crop", 10)== 0) {
+    scale_crop(
+      configuration.filenames[0],
+      atoi(configuration.arguments[0]),
+      atoi(configuration.arguments[1]),
+      atoi(configuration.arguments[2]),
+      atoi(configuration.arguments[3])
+    );
+  }
+
   /*
    * TO COMPLETE
    */
